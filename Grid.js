2a3fffd9d67a3e1096016431c8d4f03b4c683232
Alan Mayhew <alanlmayhew@gmail.com>
2016-12-14 04:36:26 -0500

    Redesigned the Grid representation and logic

--- Grid.js
+++ Grid.js
@@ -5,96 +5,99 @@
    representation of the game state
 
  +---+---+---+---+           [ 
- |   |   |   |   |         0  [ null, null, null, {obj}  ]
+ |   |   |   |   |         0  [ {3} ]
  +---+---+---+---+
- |   |   |   |   |         1  [ null, null, {obj}, {obj} ]
+ |   |   |   |   |         1  [ {1}, {4} ]
  +---+---+---+---+  ---->     
- |   | o |   |   |         2  [ null, null, null, null   ]
+ |   | 4 |   |   |         2  [ ]
  +---+---+---+---+
- | o | o |   | o |         3  [ null, null, null, {obj}  ]
+ | 3 | 1 |   | 2 |         3  [ {2} ]
  +---+---+---+---+                                        ]
  
-                            Grid.columns goes left (low index) to right (high)
-                            Columns go top (low index) to bottom (high)
+- Grid.columns goes left (low index) to right (high).
+- Columns are a stack of objects where lower index
+    means lower row.
+
  */
 
 function Grid(dim){
-    this.columns = [];
+    this.columns = new Array(dim);
+    this.columns.fill([]);
     this.dim = dim;
-    for (var i=0; i<dim; ++i){
-        var a = new Array(dim);
-        a.fill(null);
-        columns.push(a);
-    }
-    // this.tops = new Array(dim);
-    // this.tops.fill(0);
 }
 
 // attempts to drop the piece in
 // returns whether or not the piece could be successfully dropped
 Grid.prototype.dropInColumn = function(col_num, piece){
-    // find the slot
     var col = this.columns[col_num];
-    var cell = col[0];
-    var i = this.dim-1;
-    while (cell != null && i >= 0){
-        cell = col[--i];
-    }
-    if (i < 0){
-        return false;
-    }
-    // drop it
-    this.columns[col_num][i] = piece;
+    if (col.length >= dim) return false;
+    col.push(piece);
     return true;
 }
 
 // returns the number of groups removed
 Grid.prototype.findAndRemoveGroups = function(stage){
-    var groupVals = [];
-    var cell = null;
-    var row = 0;
-    var col = 0;
-    var matchingCells = [];
     var numRemoved = 0;
-    // loop through rows and columns (horizontally and vertically)
-    for (var direction=0; direction<2; ++direction){
-        for (var i=0; i<this.dim; ++i){
-            groupVals = [];
-            for (var j=0; j<this.dim; ++j){
-                if (direction == 0){
-                    row = i;
-                    col = j;
-                } else {
-                    row = j;
-                    col = i;
-                }
-                cell = this.columns[col][row];
-                // if there's a piece there update the group, otherwise check
-                // for matches and reset the group (if the group wasn't empty)
-                if (cell != null){
-                    groupVals.push(cell.numberVal);
-                    continue;
-                } else {
-                    var len = groupVals.length;
-                    if (len == 0) continue;
-                    for (var n=0; n<len; ++n){
-                        if (groupVals[n] == len){
-                            matchingCells.push([col-len+1,row]);
-                        }
-                    }
-                    groupVals = [];
-                }
+    var matchingCells = [];
+    var cell = null;
+    // a list of the column sizes, for checking for horizontal groups
+    var lengthList = [];
+
+    // check the columns, and also build a list of column lengths for later
+    for (var c=0; c<this.dim; ++c){
+        var col = this.columns[c];
+        var len = col.length;
+        lengthList.push(len);
+        for (var r=0; r<len; ++r){
+            cell = col[r];
+            if (cell.numberVal == len){
+                // matchingCells.push([c,r]);
+                matchingCells.push(c*this.dim + r);
             }
-        } 
+        }
     }
-    // TODO: make sure cell isn't null before clearing it (cells can
-    // potentially be "marked" twice)
+
+    // row checking
+    var topRow = Math.max.apply(Math, lengthList);
+    for (var r=0; r<topRow; ++r){
+        var groupVals = [];
+        for (var c=0; r<this.dim; ++c){
+            var col = this.columns[c];
+            cell = null;
+            if (lengthList[c] < r){
+                cell = col[r];
+                groupVals.push(cell.numberValue);
+            }
+            // if we just found the end of a group (or hit the end of the
+            // board, check the group and flag for removal)
+            if (cell == null || c == this.dim-1){
+                var len = groupVals.length;
+                for (var n=0; n<len; ++n){
+                    if (groupVals[n] == len){
+                        var nCol = c-len+n + (cell == null ? 0 : 1);
+                        // matchingCells.push([nCol,r]);
+                        matchingCells.push(nCol*this.dim + r);
+                    }
+                }
+                groupVals = [];
+            }
+        }
+    }
+    // to actually remove the cells, first we sort it in descending order with
+    // column taking priority over row (some div/mod shenanigans to make it
+    // easy) and then we can safely remove things from the column lists without
+    // causing issues with subsequent indices not pointing to the correct spot
+    matchingCells.sort(function(a,b){return b-a});
+    var prev = -1;
     for (var i=0; i<matchingCells.length; ++i){
-        var col = matchingCells[i][0];
-        var row = matchingCells[i][1];
+        var val = matchingCells[i];
+        if (val == prev) continue;
+        prev = val;
+        var col = Math.floor(val/this.dim);
+        var row = val % this.dim;
         cell = this.columns[col][row];
-        if (cell == null) continue;
         stage.removeChild(cell);
+        this.columns[col].splice(row,1);
         ++numRemoved;
     }
     return numRemoved;
